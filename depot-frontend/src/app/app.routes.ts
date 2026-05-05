cb0605e0ca2da712e3f70a8596ad513be13835ce
Nguyễn Gia Đại <jadennguyen@MacBook-Pro-cua-Jaden.local>
2026-05-05 10:34:31 +0700

    feat: add frontend routing

--- depot-frontend/src/app/app.routes.ts
+++ depot-frontend/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Containers } from './pages/containers/containers';
+import { ImportContainer } from './pages/import-container/import-container';
+import { ExportContainer } from './pages/export-container/export-container';
+import { Statistics } from './pages/statistics/statistics';
+import { MasterData } from './pages/master-data/master-data';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'containers',
+    component: Containers,
+  },
+  {
+    path: 'import-container',
+    component: ImportContainer,
+  },
+  {
+    path: 'export-container',
+    component: ExportContainer,
+  },
+  {
+    path: 'statistics',
+    component: Statistics,
+  },
+  {
+    path: 'master-data',
+    component: MasterData,
+  },
+];
